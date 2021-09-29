f8dd7910d275203ca60e19715a431ee0e5e54fc7
Ramandeep <ramank6238299@gmail.com>
2021-09-29 13:09:18 +0530

    start

---
+++ index.js
@@ -0,0 +1,4 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("cash-given");
+const checkbutton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
